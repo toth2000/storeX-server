7d10a5a7c5578d5d7d8bb3bef181d09a512b09f3
toth2000 <tothagata.bhattacharjee@gmail.com>
2022-03-12 19:10:38 +0530

    Change for testing

--- index.js
+++ index.js
@@ -31,12 +31,20 @@ app.get("/", (req, res) => {
   res.send("StoreX Server Running");
 });
 
-mongoose
-  .connect(process.env.MongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server is Running at Port: ${PORT}`))
-  )
-  .catch((error) => console.log("Error connecting to the database: ", error));
+
+console.log("Env file check");
+console.log("MongoUrl", process.env.MongoUrl);
+console.log("PASS_SALT", process.env.PASS_SALT);
+console.log("JWT_SALT", process.env.JWT_SALT);
+console.log("razorpay_id", process.env.razorpay_id);
+console.log("razorpay_secret", process.env.razorpay_secret);
+
+// mongoose
+//   .connect(process.env.MongoURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() =>
+//     app.listen(PORT, () => console.log(`Server is Running at Port: ${PORT}`))
+//   )
+//   .catch((error) => console.log("Error connecting to the database: ", error));
